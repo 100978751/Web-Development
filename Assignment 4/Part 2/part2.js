d2a6c4ee363e3c5fd9c681f667309763e2c88dc2
100978751 <sujay.tailor@dcmail.ca>
2025-04-03 18:07:16 -0400

    part 1 of js

---
+++ Assignment 4/Part 2/part2.js
@@ -0,0 +1,5 @@
+// === Part 1: DOM Elements ===
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
