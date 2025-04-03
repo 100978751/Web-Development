a72b58a368def4570acdf55d89cd3f2176418e7d
100978751 <sujay.tailor@dcmail.ca>
2025-04-03 18:21:38 -0400

    part2 of js

--- Assignment 4/Part 3/part4.js
+++ Assignment 4/Part 3/part4.js
@@ -4,3 +4,12 @@ const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
+
+// === Part 2: Utility Functions ===
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  function randomRGB() {
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+  }
